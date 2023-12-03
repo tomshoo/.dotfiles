8223d1b31b7b04351808f67bf577bb1797557968
Shubhanshu Tomar <57960301+tomshoo@users.noreply.github.com>
2023-12-03 22:51:18 +0530

    update script to generate image for swaylock

--- river/scripts/wallpaper.sh
+++ river/scripts/wallpaper.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+set-wallpaper() {
+    wallpaper_file="$1"
+
+    swww img "$wallpaper_file" --transition-type simple --transition-step 20 &
+
+    [[ -f /tmp/current_wallpaper.jpg ]] && mv /tmp/current_wallpaper.jpg /tmp/current_wallpaper.jpg.backup
+
+    if ffmpeg -i "$wallpaper_file" /tmp/current_wallpaper.jpg; then
+        [[ -f /tmp/current_wallpaper.jpg.backup ]] && rm /tmp/current_wallpaper.jpg.backup
+    else
+        [[ -f /tmp/current_wallpaper.jpg.backup ]] && mv /tmp/current_wallpaper.jpg.backup /tmp/current_wallpaper.jpg
+    fi
+}
+
 if ! pidof swww-daemon; then
     swww init
     image=$(swww query | cut -d' ' -f8)
