ed1aeb7b2423fcb10ee52825d2ada4603911a978
Shubhanshu Tomar <57960301+tomshoo@users.noreply.github.com>
2023-12-03 22:47:49 +0530

    river: script code cleanup

--- river/scripts/volume.sh
+++ river/scripts/volume.sh
@@ -2,29 +2,27 @@
 
 while getopts d:i:m: flag; do
     case "${flag}" in
-    d)  pactl set-sink-volume @DEFAULT_SINK@ "-${OPTARG}%"
-        break
-        ;;
-    i)  pactl set-sink-volume @DEFAULT_SINK@ "+${OPTARG}%"
-        break
-        ;;
-    m)  pactl set-sink-mute @DEFAULT_SINK@ toggle
-        if [ "$(pamixer --get-volume-human)" == "muted" ]; then
-            notify-send                                         \
-                -h string:x-canonical-private-synchronous:audio \
-                -a "com.desktop.River"                          \
-                "Volume: "                                      \
-                "$(pamixer --get-volume-human)"
-            exit 0
-        fi
-        ;;
+    d) pactl set-sink-volume @DEFAULT_SINK@ "-${OPTARG}%"
+       break ;;
+    i) if ! [[ $(pamixer --get-volume) -ge 150 ]]; then
+        pactl set-sink-volume @DEFAULT_SINK@ "+${OPTARG}%"
+       fi
+       break ;;
+    m) pactl set-sink-mute @DEFAULT_SINK@ toggle
+       break ;;
     *) ;;
     esac
 done
 
-percentage=$(pamixer --get-volume-human)
+p_arg=()
+volume=$(pamixer --get-volume-human)
+
+if ! [[ "$volume" = muted ]]; then
+    p_arg=(-h int:value:"$volume")
+fi
+
 notify-send                                         \
-    -h int:value:"${percentage}"                    \
+    "${p_arg[@]}"                                   \
     -h string:x-canonical-private-synchronous:audio \
     -a "com.desktop.River"                          \
-    "Volume: ${percentage}"
+    "Volume: ${volume}"
