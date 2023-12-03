001b96b8fc37a6be31024c721693cdb933eb4ae3
Shubhanshu Tomar <57960301+tomshoo@users.noreply.github.com>
2023-12-03 22:48:23 +0530

    add new option to move window to workspace with focus

--- river/scripts/workspace.sh
+++ river/scripts/workspace.sh
@@ -1,4 +1,6 @@
-while getopts g:m:s:t: flag; do
+#!/usr/bin/env sh
+
+while getopts g:m:s:t:S: flag; do
     case "${flag}" in
     g) tag_id=$(( (1 << (OPTARG - 1)) + (1 << 30) ))
        riverctl set-focused-tags "${tag_id}"
@@ -12,6 +14,12 @@ while getopts g:m:s:t: flag; do
     t) tag_id=$(( 1 << (OPTARG - 1) ))
        riverctl toggle-focused-tags "${tag_id}"
        ;;
-    *) echo "Usage: $0 -[g|m|s]"; exit 1 ;;
+    S) tag_id=$(( 1 << (OPTARG - 1) ))
+       riverctl set-view-tags "$tag_id" 
+       riverctl set-focused-tags "$(( tag_id + (1 << 30) ))"
+       ;;
+    *) echo "Usage: $0 -[g|m|s]";
+       exit 1
+       ;;
     esac
 done
